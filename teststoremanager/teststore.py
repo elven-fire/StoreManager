063767beb0ec482a8d29c4d657fc6ea84437f472
Rini Roscoe <rini.roscoe@gmail.com>
2021-01-23 23:08:56 -0600

    Validate weapon style for random items

--- teststoremanager/teststore.py
+++ teststoremanager/teststore.py
@@ -5,8 +5,9 @@ from storemanager.locations.store import *
 class TestSpecificWeaponStore(unittest.TestCase):
 
     def test_valid(self):
-        """Initialize a specific weapons store, validate no errors."""
+        """Initialize a specific weapons store, validate all items are of the correct style."""
         store = SpecificWeaponStore("Special Weapons", 5, "Sword")
+        self.assertTrue(all([item.style == "Sword" for item in store.inventory]))
 
     def test_invalid_style(self):
         """Provide an invalid style, validate error thrown."""
